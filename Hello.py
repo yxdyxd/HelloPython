f0e1ee9163af449e04e54702c569c13ba13a7555
杨旭东 <240933281@qq.com>
2018-07-11 16:45:48 +0800

    company

--- Hello.py
+++ Hello.py
@@ -4005,7 +4005,7 @@
 
 # TCP编程
 # 创建一个基于TCP连接的Socket
-import socket, threading, time
+# import socket, threading, time
 
 # 创建一个socket
 # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -4041,7 +4041,6 @@ import socket, threading, time
 #     f.write(html)
 
 
-
 # 服务器
 # 服务器进程首先要绑定一个端口并监听来自其他客户端的连接。
 # 如果某个客户端连接过来了，服务器就与该客户端建立Socket连接，
@@ -4050,43 +4049,156 @@ import socket, threading, time
 # 一个Socket依赖4项：服务器地址、服务器端口、客户端地址、客户端端口来唯一确定一个Socket。
 # 编写一个简单的服务器程序，它接收客户端连接，把客户端发过来的字符串加上Hello再发回去
 # 首先，创建一个基于IPv4和TCP协议的Socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 端口号需要预先指定。因为我们写的这个服务不是标准服务，所以用9999这个端口号。
 # 请注意，小于1024的端口号必须要有管理员权限才能绑定：
 
 # 监听端口号
 # 127.0.0.1是一个特殊的IP地址，表示本机地址，如果绑定到这个地址，
 # 客户端必须同时在本机运行才能连接，也就是说，外部的计算机无法连接进来。
-s.bind(('127.0.0.1', 9999))
+# s.bind(('127.0.0.1', 9999))
 # 紧接着，调用listen()方法开始监听端口，传入的参数指定等待连接的最大数量：
-s.listen(5)
-print('Waiting for connection...')
+# s.listen(5)
+# print('Waiting for connection...')
 # 接下来，服务器程序通过一个永久循环来接受来自客户端的连接，
 # accept()会等待并返回一个客户端的连接:
-while True:
-    # 接受一个新的连接
-    sock, addr = s.accept()
-    # 创建新线程来处理TCP连接
-    t = threading.Thread(target=tcplink, args=(sock, addr))
-    t.start()
+# while True:
+# 接受一个新的连接
+# sock, addr = s.accept()
+# 创建新线程来处理TCP连接
+# t = threading.Thread(target=tcplink, args=(sock, addr))
+# t.start()
 
 
 # 每个连接都必须创建新线程（或进程）来处理，
 # 否则，单线程在处理连接的过程中，无法接受其他客户端的连接：
-    def tcplink(sock, addr):
-        print('Accept new connection from %s:%s...' % addr)
-        sock.send(b'Welcome')
-        while True:
-            data = sock.recv(1024)
-            time.sleep(1)
-            if not data or data.decode('utf-8') == 'exit':
-                break
-            sock.send(('Hello, %s' % data.decode('utf-8')).encode('utf-8'))
-        sock.close()
-        print('Connection from %s:%s closed.' % addr)
+#     def tcplink(sock, addr):
+#         print('Accept new connection from %s:%s...' % addr)
+#         sock.send(b'Welcome')
+#         while True:
+#             data = sock.recv(1024)
+#             time.sleep(1)
+#             if not data or data.decode('utf-8') == 'exit':
+#                 break
+#             sock.send(('Hello, %s' % data.decode('utf-8')).encode('utf-8'))
+#         sock.close()
+#         print('Connection from %s:%s closed.' % addr)
 
 # 连接建立后，服务器首先发一条欢迎消息，然后等待客户端数据，
 # 并加上Hello再发送给客户端。如果客户端发送了exit字符串，就直接关闭连接。
 
+# import socket
+# TCP是建立可靠连接，并且通信双方都可以以流的形式发送数据。
+# 相对TCP，UDP则是面向无连接的协议。
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定端口
+# s.bind(('127.0.0.1', 9999))
+# 创建Socket时，SOCK_DGRAM指定了这个Socket的类型是UDP。
+# 绑定端口和TCP一样，但是不需要调用listen()方法，而是直接接收来自任何客户端的数据：
+# recvfrom()方法返回数据和客户端的地址与端口，
+# 这样，服务器收到数据后，直接调用sendto()就可以把数据用UDP发给客户端。
+# print('Bind UDP on 9999...')
+# while True:
+#     # 接受数据
+#     data, addr = s.recvfrom(1024)
+#     print('Received from %s:%s.' % addr)
+#     s.sendto(b'Hello, %s!' % data, addr)
 
 
+# 客户端使用UDP时，首先仍然创建基于UDP的Socket，
+# 然后，不需要调用connect()，直接通过sendto()给服务器发数据：
+# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# for data in [b'Michael', b'Tracy', b'Sarah']:
+#     s.sendto(data, ('127.0.0.1', 9999))
+#     print(s.recvfrom(1024).decode('utf-8'))
+# s.close()
+
+# from email.mime.text import MIMEText
+
+# SMTP是发送邮件的协议，Python内置对SMTP的支持，
+# 可以发送纯文本邮件、HTML邮件以及带附件的邮件。
+
+# Python对SMTP支持有smtplib和email两个模块，
+# email负责构造邮件，smtplib负责发送邮件。
+
+# 首先，我们来构造一个最简单的纯文本邮件：
+# 注意到构造MIMEText对象时，第一个参数就是邮件正文，第二个参数是MIME的subtype，
+# 传入'plain'表示纯文本，最终的MIME就是'text/plain'，
+# 最后一定要用utf-8编码保证多语言兼容性。
+# msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# 然后，通过SMTP发出去：
+# 输入Email地址和口令
+# from_addr = input('From: ')
+# password = input('Password: ')
+# 输入收件人地址
+# to_addr = input('To: ')
+# 输入SMTP服务器地址
+# smtp_server = input('SMTP server: ')
+
+# import smtplib
+
+# SMTP协议默认端口为25
+# 我们用set_debuglevel(1)就可以打印出和SMTP服务器交互的所有信息。
+# SMTP协议就是简单的文本命令和响应。
+# login()方法用来登录SMTP服务器，
+# sendmail()方法就是发邮件，由于可以一次发给多个人，所以传入一个list，
+# 邮件正文是一个str，as_string()把MIMEText对象变成str。
+# server = smtplib.SMTP(smtp_server, 25)
+# server.set_debuglevel(1)
+# server.login(from_addr, password)
+# server.sendmail(from_addr, [to_addr], msg.as_string())
+# server.quit()
+
+# 把From、To和Subject添加到MIMEText中，才是一封完整的邮件：
+from email import encoders
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+
+import smtplib, socket
+
+
+# def _format_addr(s):
+#     name, addr = parseaddr(s)
+#     return formataddr(Header(name, 'utf-8').encode(), addr)
+#
+#
+# from_addr = input('From: ')
+# password = input('Password: ')
+# to_addr = input('To: ')
+# smtp_server = input('SMTP server: ')
+#
+# msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+# msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+# msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+# msg['Subject'] = Header('来自SMTP的问候....', 'utf-8').encode()
+#
+# server = smtplib.SMTP_SSL(smtp_server, 465)
+# server.set_debuglevel(1)
+# server.login(from_addr, password)
+# server.sendmail(from_addr, [to_addr], msg.as_string())
+# server.quit()
+
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
+
+# socket.gethostbyname("127.0.0.1")
+from_addr = input('From: ')
+password = input('Password: ')
+to_addr = input('To: ')
+smtp_server = input('SMTP server: ')
+
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['From'] = _format_addr('Python爱好者 <%s>' % from_addr)
+msg['To'] = _format_addr('管理员 <%s>' % to_addr)
+msg['Subject'] = Header('来自SMTP的问候……', 'utf-8').encode()
+
+server = smtplib.SMTP_SSL(smtp_server, 465)
+server.starttls()
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
---
+++ TCP_Client.py
@@ -0,0 +1,10 @@
+import socket
+import threading
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1',9999))
+print(s.recv(1024).decode('utf-8'))
+for data in [b'michael',b'tracy',b'sarah']:
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
